63329760a075b5eac67aa11a4ddbcd96d4920038
xiaonanpols21 <xiaonanpolspols@gmail.com>
2022-04-28 10:56:49 +0200

    week-1-back-end

---
+++ server.js
@@ -0,0 +1,17 @@
+console.log("Hallo world");
+
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.get('/test', (req, res) => {
+  res.send('Dit is test!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
