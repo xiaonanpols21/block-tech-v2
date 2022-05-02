3fbff00b0a4bbe581753059b504d1d1bdb41de69
xiaonanpols21 <xiaonanpolspols@gmail.com>
2022-05-02 11:16:54 +0200

    form zonder fav drama en people

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.use(express.static('public'));
 app.set('view engine', 'ejs');
 
 // All data comes fron: https://mydramalist.com/
-const genre = ["romance", "historical", "music", "action", "adventure", "melodrama", "idoldrama", "comedy", "friendship", "business"];
+const genres = ["romance", "historical", "music", "action", "adventure", "melodrama", "idoldrama", "comedy", "friendship", "business"];
 const kdramas = [
   {
     "id": 1,
