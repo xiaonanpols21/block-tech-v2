b8e0fa1ba03ff36dbde1fa8eebccb05719eed070
xiaonanpols21 <xiaonanpolspols@gmail.com>
2022-05-17 20:11:48 +0200

    api back-end, fetch not defined

--- server.js
+++ server.js
@@ -7,7 +7,28 @@ const arrayify = require("array-back");
 const mongoose = require("mongoose");
 
 // JS files
-const kdramaData = require('./data/kdrama-data.js');
+const kdramaData = require("./data/kdrama-data.js");
+
+// API key
+function apiCall() {
+  const BASE_URL = "https://api.themoviedb.org/3";
+  const API_KEY = process.env.API_KEY;
+  // Bron: https://stackoverflow.com/questions/4870328/read-environment-variables-in-node-js#:~:text=To%20retrieve%20environment%20variables%20in,assigning%20a%20property%20on%20process.
+  const url =
+    BASE_URL +
+    "/discover/tv?" +
+    "api_key=" +
+    API_KEY +
+    "&language=en-US&sort_by=popularity.desc&page=1&primary_release_year=2020&with_original_language=hi|ko|";
+
+  // API fetchen met Promise
+  return fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+      // console.log(data);
+      return data.results;
+    });
+}
 
 // Wachtwoord voor MongoDB
 require("dotenv").config();
@@ -21,11 +42,11 @@ async function main() {
   try {
     await mongoose.connect(process.env.mongoBeauty);
     console.log("success");
-  } catch(error) {
-    throw error
+  } catch (error) {
+    throw error;
   }
 }
-main()
+main();
 
 // Data posten
 app.use(express.json());
@@ -106,42 +127,45 @@ app.get("/detail", (req, res) => {
 
 const mylist = [
   {
-    name: "All of us are dead"
-  }
-]
+    name: "All of us are dead",
+  },
+];
 
-app.get("/mylist", (req, res) => {
+app.get("/mylist", async function (req, res) {
+  console.log("mylist");
+  // console.log(mylist)
+  const apiData = await apiCall();
+  console.log(apiData[0].name);
+
   res.render("pages/mylist", {
     users: kdramaData.users,
+    apiData: apiData,
     mylist,
-  })
-  
+  });
 });
 
 app.post("/mylist", (req, res) => {
   console.log(req.body);
 
-  data.push({
-
-  });
+  data.push({});
 });
 
-app.get("/mylist/:id", (req, res)=> {
+app.get("/mylist/:id", (req, res) => {
   console.log(req.params.id);
-  const kdrama = kdrama.find( element => element.id == req.params.id)
-  console.log(kdrama)
+  const kdrama = kdrama.find((element) => element.id == req.params.id);
+  console.log(kdrama);
   // res.send("hij doet het");
   res.render("pages/detail.ejs", {
     genre: kdramaData.genre,
     kdramas: kdramaData.kdramas,
     storyLine: kdramaData.storyLine,
-  })
+  });
 });
 
 app.use(function (req, res) {
   console.error("Error 404: page nog found");
   res.status(404).render("pages/404", {
-    kdramas: kdramaData.kdramas
+    kdramas: kdramaData.kdramas,
   });
 });
 
@@ -153,12 +177,12 @@ const kdramaSchema = new mongoose.Schema({
   slug: String,
   year: Number,
   genre: Array,
-  storyline: String
+  storyline: String,
 });
 
-const Kdrama = mongoose.model('Kdrama', kdramaSchema);
+const Kdrama = mongoose.model("Kdrama", kdramaSchema);
 
-async function newKdrama () {
+async function newKdrama() {
   const hymn = new Kdrama({
     slug: "the-hymn-of-death",
     name: "The Hymn of Death",
@@ -166,10 +190,10 @@ async function newKdrama () {
     genre: ["musical", "historical", "romance", "melodrama"],
     storyline:
       "Kim Woo Jin is a stage drama writer while Korea is under Japanese occupation. He is married, but he falls in love with Yun Shim Deok. Shim Deok is the first Korean soprano. She records the song “Praise of Death” which becomes the first Korean pop song in 1926. Woo Jin and Shim Deok's fate ends tragically.",
-  })
-  await hymn.save()
+  });
+  await hymn.save();
 }
-newKdrama()
+newKdrama();
 
 /*
 app.get("/kdrama/:id/:slug", (req, res) => {
