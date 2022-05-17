2fa5bbd6a075b502f93b5b578412e2796c7d1a42
xiaonanpols21 <xiaonanpolspols@gmail.com>
2022-05-17 11:54:03 +0200

    mylist

--- public/js/mylist.js
+++ public/js/mylist.js
@@ -1,7 +1,7 @@
 console.log("Mylist");
 
 // Data aanvragen
-const API_KEY = "api_key=c9c582007e770d9564a6499f6e364a2a";
+ const API_KEY = "c9c582007e770d9564a6499f6e364a2a";
 // const API_KEY = process.env.API_Key;
 // Bron: https://stackoverflow.com/questions/4870328/read-environment-variables-in-node-js#:~:text=To%20retrieve%20environment%20variables%20in,assigning%20a%20property%20on%20process.
 
@@ -9,6 +9,7 @@ const BASE_URL = "https://api.themoviedb.org/3";
 const API_URL =
   BASE_URL +
   "/discover/tv?" +
+  "api_key=" +
   API_KEY +
   "&language=en-US&sort_by=popularity.desc&page=1&primary_release_year=2020&with_original_language=hi|ko|";
 const IMG_URL = "https://image.tmdb.org/t/p/w500";
